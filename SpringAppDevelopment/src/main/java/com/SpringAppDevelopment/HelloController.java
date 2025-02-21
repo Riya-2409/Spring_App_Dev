46dbb582fbed2d8bb2a76ff4abfb5cd0430674c9
Riya <soniriya2409@gmail.com>
2025-02-21 19:48:20 +0530

    [Riya] Add. Add Logger to Log Messages

--- SpringAppDevelopment/src/main/java/com/SpringAppDevelopment/HelloController.java
+++ SpringAppDevelopment/src/main/java/com/SpringAppDevelopment/HelloController.java
@@ -1,5 +1,8 @@
 package com.SpringAppDevelopment;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,10 +11,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/hello")
 public class HelloController {
 
+    //Correct SLF4J Logger
+    private static final Logger logger = LoggerFactory.getLogger(HelloController.class);
+
+
     //UC1: Simple Hello World
     @GetMapping("/uc1")
     public String sayHelloUC1() {
         return "Hello from BridgeLabz";
     }
+
+    //UC2: Hello World with Logging
+    @GetMapping("/uc2")
+    public String sayHelloUC2() {
+        logger.info("INFO: sayHellouc2() method called");
+        logger.debug("DEBUG: Returning hello message");
+        return "Hello from BridgeLabz with Logging";
+    }
 }
+
+
 
